947e1ceb4a5ed28012b2943d994f59e24def6f7e
Sarah Buchhorn <SBuchhorn@cmap.illinois.gov>
2020-02-04 12:10:11 -0600

    -added file formats -working on text/grid

--- R/finalize_plot.R
+++ R/finalize_plot.R
@@ -1,30 +1,68 @@
 #' Allow user to export plot in desired format
 #'
 #' @export
-finalize_plot <- function(savepath, type, dpi=96) {
+save_plot <- function(savepath, type) {
 
   if (type == 'web'){
     # width should be 670 pixels, but units must be in in, cm, or mm
     # will depend on the resolution
+    dpi = 96
     pxin = dpi
     width = 670/pxin
     units = "in"
     text1 = 11
     text2 = 14
     text3 = 17
+    device = 'svg'
+  }
+
+  if (type == 'report'){
+    # width should be 670 pixels, but units must be in in, cm, or mm
+    # will depend on the resolution...doesn't make sense at 300 ppi
+    dpi = 300
+    pxin = 72  # setting at 72 for now
+    width = 670/pxin
+    units = "in"
+    text1 = 11
+    text2 = 14
+    text3 = 17
+    device = 'tiff'
+  }
+
+  if (type == 'PowerPoint'){
+    # width should be 670 pixels, but units must be in in, cm, or mm
+    # will depend on the resolution
+    dpi = 300
+    pxin = 72  # setting at 72 for now
+    width = 670/pxin
+    units = "in"
+    text1 = 11
+    text2 = 14
+    text3 = 17
+    device = 'tiff'
   }
 
   ggsave(savepath,
          width = width,
-         units = units)
+         units = units,
+         device = device)
 }
 
+#' @export
+create_footer <- function() {
+  footer <- grid::grobTree(grid::textGrob('hello'))
+  return(footer)
+}
+
+footer <- create_footer()
+
 # EXAMPLE
-  ggplot(grp_over_time, aes(x = year, y = realgrp, color = cluster, label = cluster)) +
-  geom_line() + theme_cmap() + geom_text_lastonly()
+#   myplot <- ggplot(grp_over_time, aes(x = year, y = realgrp, color = cluster, label = cluster)) +
+#   geom_line() + theme_cmap() + geom_text_lastonly()
+#
+#   plot_grid <- ggpubr::ggarrange(myplot, footer)
+#
+# # ggsave('C:/Users/sbuchhorn/Desktop/gg/ggtest.png')
+# #
+# save_plot('C:/Users/sbuchhorn/Desktop/gg/ggtest.tiff', 'PowerPoint')
 
-ggsave('C:/Users/sbuchhorn/Desktop/gg/ggtest.png')
-
-finalize_plot('C:/Users/sbuchhorn/Desktop/gg/ggtest.png', 'web')
-
-# so like, what will this do that ggsave doesn't already do
