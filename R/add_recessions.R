5cd3857289ea803824bde7c447cdf13effc68366
Matthew Stern <54633946+tallishmatt@users.noreply.github.com>
2020-02-18 17:37:37 -0600

    many changes
    code is complete. integer breaks now own R file. Many man pages improved. However, devtools::check still fails due to issues in man page examples.

--- R/add_recessions.R
+++ R/add_recessions.R
@@ -1,10 +1,96 @@
-#' Add recessions to time series as background rectangles
-#' https://cran.r-project.org/web/packages/ggplot2/vignettes/extending-ggplot2.html
-#' Note: color approximates #e3e8eb. hex code and alpha calculated with help from
-#' https://stackoverflow.com/questions/6672374/convert-rgb-to-rgba-over-white
+#' Add recessions to time series graphs
+#'
+#' \code{add_recessions} returns ggplot geoms that add rectangles representing
+#' recessions to a plot. It will either return a single \code{geom_rect} geom or
+#' \code{geom_rect} and \code{geom_text} geoms, where the text places the word
+#' "recession" near each rect.
+#'
+#' @usage add_recessions(min = 2000, max = 2010, xdata = c("int", "date"), text
+#'   = TRUE, text_nudge_x = .2, fill = "#002d49", alpha = 0.11)
+#'
+#' @param min Numeric, the year of the earliest recession to display.
+#' @param max Numeric, the year of the latest recession to display.
+#' @param xdata Char, a string indicating whether the x axis of the primary data
+#'   being graphed is in integer or date format. This argument will accept any
+#'   of the following values: \code{c("int", "integer", "integers", "i", "date",
+#'   "dates", "d")}
+#' @param text Logical, whether or not to include labels that identify each box
+#'   as a recession.
+#' @param text_nudge_x Numeric, the amount to shift the labels along the x axis.
+#'   Measured as a distance from the end of each recession (a value of 0 will
+#'   place the label on the edge of each rectangle). The default of 0.2 seems to
+#'   work well in many situations but may need to be overridden.
+#' @param fill Char, the color of the rectangles to draw.
+#' @param alpha Numeric, the opacity of the rectangles to draw.
+#'
+#' @section Default color: The CMAP color palette gray used for recessions is
+#'   \code{#e3e8eb}. The default fill and alpha values of \code{#002d49} and
+#'   \code{0.11} replicate the palette color at the highest possible
+#'   transparency. This is done because there is no known way to place the
+#'   recession geom behind the graph's grid lines. The default therefore
+#'   produces the approved CMAP color while altering the appearance of any
+#'   overlapping grid lines as little as possible. This was generated using the
+#'   hints found here:
+#'   \url{https://stackoverflow.com/questions/6672374/convert-rgb-to-rgba-over-white}.
+#'
+#' @import dplyr
+#' @importFrom lubridate date_decimal
+#'
+#'
+#' @examples
+#' grp_goods <- dplyr::mutate(dplyr::filter(grp_over_time, grp_over_time, category == "Goods-Producing"), year2 = as.Date(date_decimal(year)))
+#'
+#' # INTEGER X AXIS:
+#' ggplot(data = grp_goods,
+#' mapping = aes(x = year, y = realgrp, color = cluster)) +
+#'   add_recessions(min = 2007, max = 2019, xdata = "int", text = TRUE) +
+#'   geom_line() +
+#'   scale_x_continuous("Year") +
+#'   theme_minimal()
+#'
+#' # DATE X AXIS:
+#' ggplot(data = grp_goods,
+#' mapping = aes(x = year2, y = realgrp, color = cluster)) +
+#'   add_recessions(min = 2007, max = 2019, xdata = "date", text = TRUE, text_nudge_x = 100) +
+#'   geom_line() +
+#'   scale_x_date("Year") +
+#'   theme_minimal()
+#'
+#' # BELOW EXAMPLES SHOW MORE THAN 1 RECESSION
+#' \dontrun{
+#' library(tidyverse)
+#'
+#' time_series <- tibble(date_dec = 1800:2020, var = rnorm(221), var2 = rnorm(221)) %>%
+#' pivot_longer(cols = starts_with("var"),
+#'              names_to = "var") %>%
+#'   mutate(date_date = as.Date(lubridate::date_decimal(date_dec))) %>%
+#'   select(date_dec, date_date, var, value)
+#'
+#' # A plot with an integer-based x axis
+#' ggplot(data = filter(time_series, date_dec >= 1980 & date_dec <= 2019),
+#'        mapping = aes(x = date_dec, y = value, color = var)) +
+#'   add_recessions(min = 1980, max = 2019, xdata = "int", text = TRUE) +
+#'   geom_line() +
+#'   scale_x_continuous("Year") +
+#'   theme_cmap()
+#'
+#' # A plot with a date-based x axis
+#' ggplot(data = filter(time_series, date_dec >= 1980 & date_dec <= 2019),
+#'        mapping = aes(x = date_date, y = value, color = var)) +
+#'   add_recessions(min = 1980, max = 2019, xdata = "date", text = TRUE) +
+#'   geom_line() +
+#'   scale_x_date() +
+#'   theme_cmap()
+#' }
+#'
+#' @seealso
+#'
+#' \url{https://htmlpreview.github.io/?https://github.com/brodieG/ggbg/blob/development/inst/doc/extensions.html#stat-compute}
+#' \url{https://rpubs.com/hadley/97970}
+#' \url{https://cran.r-project.org/web/packages/ggplot2/vignettes/extending-ggplot2.html}
 #'
 #' @export
-add_recessions <- function(min = 2000, max = 2010, xdata = c("int", "date"), fill = "#002d49", alpha = 0.11, text = TRUE, text_nudge_x = .2){
+add_recessions <- function(min = 2000, max = 2010, xdata = c("int", "date"), text = TRUE, text_nudge_x = .2, fill = "#002d49", alpha = 0.11){
 
   # handle multiple constructions of `xdata`
   if (xdata %in% c("i", "int", "integer", "integers")){
@@ -53,50 +139,4 @@ add_recessions <- function(min = 2000, max = 2010, xdata = c("int", "date"), fil
 
 
 
-#' clean up axis breaks where n = desired number of ticks.
-#' from https://joshuacook.netlify.com/post/integer-values-ggplot-axis/
-#' @export
-integer_breaks <- function(n = 5, ...) {
-  fxn <- function(x) {
-    breaks <- floor(pretty(x, n, ...))
-    names(breaks) <- attr(breaks, "labels")
-    breaks
-  }
-  return(fxn)
-}
-
-
-
-
-# time_series <- tibble(date_dec = 1800:2020, var = rnorm(221), var2 = rnorm(221)) %>%
-#   pivot_longer(cols = starts_with("var"),
-#                names_to = "var") %>%
-#   mutate(date_date = as.Date(lubridate::date_decimal(date_dec))) %>%
-#   select(date_dec, date_date, var, value)
-#
-#
-# # INTEGER
-# ggplot(data = filter(time_series, date_dec >= 2000 & date_dec <= 2019),
-#        mapping = aes(x = date_dec, y = value, color = var)) +
-#   add_recessions(min = 2000, max = 2019, xdata = "int", text = TRUE) +
-#   geom_line() +
-#   scale_x_continuous("Year", breaks = integer_breaks(n = 6)) +
-#   theme_cmap()
-#
-# # DATE
-# ggplot(data = filter(time_series, date_dec >= 2000 & date_dec <= 2019),
-#        mapping = aes(x = date_date, y = value, color = var)) +
-#   add_recessions(min = 2000, max = 2019, xdata = "date", text = TRUE) +
-#   geom_line() +
-#   scale_x_date() +
-#   theme_cmap()
-
-
-
-
-
-# resourceS:
-# https://htmlpreview.github.io/?https://github.com/brodieG/ggbg/blob/development/inst/doc/extensions.html#stat-compute
-# https://rpubs.com/hadley/97970
-# https://cran.r-project.org/web/packages/ggplot2/vignettes/extending-ggplot2.html
 
