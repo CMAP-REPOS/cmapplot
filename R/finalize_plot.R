0b49c857cb3ac3e58ce4e655a0b4cda7f0da03c8
Sarah Buchhorn <SBuchhorn@cmap.illinois.gov>
2020-02-24 15:10:57 -0600

    -using ggarrange as in bbplot -moved references to title, subtitle, caption out of theme_cmap()

--- R/finalize_plot.R
+++ R/finalize_plot.R
@@ -1,142 +1,54 @@
-#' Allow user to export plot in desired format
 
-prepare_title <- function(title) {
-
-  title <- grid::textGrob(title, x=0.1, y=0.9, just=c("left", "top"),
-                          gp = gpar(fontfamily=cmapplot_globals$font_title,
-                                    fontsize=18,
-                                    fontface="bold",
-                                    col="#222222"))
-  return(title)
+save_plot <- function (plot_grid, width, height, save_filepath) {
+  grid::grid.draw(plot_grid)
+  #save it
+  ggplot2::ggsave(filename = save_filepath,
+                  plot=plot_grid, width=(width/72), height=(height/72),  bg="white")
 }
 
-prepare_source <- function(source) {
+create_title_block <- function (title, subtitle) {
+  title_block <- grid::grobTree(grid::linesGrob(x = grid::unit(c(0, 1), "npc"), y = grid::unit(1.1, "npc")),
+                                grid::textGrob(title,
+                                               x = 0.1, hjust = 0, vjust = 1, y=0.97,
+                                               gp = grid::gpar(fontsize=17,
+                                                               fontfamily=cmapplot_globals$font_title,
+                                                               lineheight=1)),
+                                grid::textGrob(subtitle,
+                                               x = 0.1, hjust = 0, y = 0.51,
+                                               gp = grid::gpar(fontsize=11,
+                                                               fontfamily=cmapplot_globals$font_reg,
+                                                               lineheight=1))
+  )
 
-  source <- grid::textGrob(source, x=0.1, y=0.9, just=c("left","top"),
-                           gp = gpar(
-                             family=cmapplot_globals$font_title,
-                             fontsize=10
-                           ))
+  return(title_block)
 
-  return(source)
 }
-
-
 
 #' @export
-save_plot <- function(savepath, type) {
+draw_plot <- function(title,
+                      subtitle,
+                      save_filepath,
+                      width_pixels,
+                      height_pixels) {
 
-  if (type == 'web'){
-    # width should be 670 pixels, but units must be in in, cm, or mm
-    # depends on the resolution.  assume 72
-    dpi = 72
-    pxin = dpi
-    width = 670/pxin
-    units = "in"
-    text1 = 11
-    text2 = 14
-    text3 = 17
-    device = 'svg'
-  }
+  side <- create_title_block(title, subtitle)
 
-  if (type == 'report'){
-    # width should be 670 pixels, but units must be in in, cm, or mm
-    # depends on the resolution.  assume 72
-    dpi = 300
-    pxin = 72  # setting at 72 for now
-    width = 670/pxin
-    units = "in"
-    text1 = 11
-    text2 = 14
-    text3 = 17
-    device = 'tiff'
-  }
+  ggpubr::ggarrange(side, myplot,
+                    ncol = 2, nrow = 1,
+                    widths = c(1,2.9))
 
-  if (type == 'PowerPoint'){
-    # width should be 670 pixels, but units must be in in, cm, or mm
-    # depends on the resolution.  assume 72
-    dpi = 300
-    pxin = 72  # setting at 72 for now
-    width = 670/pxin
-    units = "in"
-    text1 = 11
-    text2 = 14
-    text3 = 17
-    device = 'tiff'
-  }
-
-  ggsave(savepath,
-         width = width,
-         units = units,
-         device = device)
+  # just returning in viewport for now, not saving.
+  # save_plot(plot_grid, width_pixels, height_pixels, save_filepath)
 }
 
 
-# I still need to deal with font sizes (including those set in theme_cmap) and text wrapping, and line spacing
-title <- prepare_title(title='Change in labor force
-size per 1,000
-residents, by age,
-Chicago and select
-Metropolitan
-Statistical Areas,
-2006-10 to 2013-17')
+# example (load economy_basic)
+myplot <- ggplot(economy_basic, aes(x = interaction(year, variable), y = value, fill = sector)) +
+  geom_col(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  theme_cmap()
 
-source <- prepare_source(source='Source: Chicago Metropolitan
-Agency for Planning analysis of
-American Community Survey
-data, five-year estimates,
-2006-10 and 2013-17')
+title <- "Change in labor force \nsize per 1,000 \nresidents, by age, \nChicago and select \nMetropolitan \nStatistical Areas, \n2006-10 to 2013-17"
+subtitle <- "Source: Chicago Metropolitan \nAgency for Planning analysis of \nAmerican Community Survey \ndata, five-year estimates, \n2006-10 and 2013-17."
 
-# The graph
-myplot <- ggplot(grp_over_time, aes(x = year, y = realgrp, color = cluster, label = cluster)) +
-  geom_line()
-# + theme_cmap()
-
-# putting it together
-# like, i don't know if I can make uneven row heights with this, and if not I'd need to somehow group all the text
-# I can go back to the viewport thing but would just have to figure out how to save
-# plot_grid <-
-ggpubr::ggarrange(title, myplot, source,
-                  ncol=2, nrow= 2,
-                  heights = c(1,1)
-                  )
-
-# save
-# save_plot('C:/Users/sbuchhorn/Desktop/gg/ggtest3.tiff', 'report')
-
-
-# EXTRA
-
-# This is using the viewport to draw/arrange
-
-title_vp <- viewport(x=0.234, y=1, width=0.234, height=0.5, just=c("right", "top"))
-pushViewport(title_vp)
-grid.draw(title)
-popViewport()
-
-source_vp <- viewport(x=0.234, y=0.5, width = 0.234, height=0.5, just=c("right", "top"))
-pushViewport(source_vp)
-grid.draw(source)
-popViewport()
-
-chart_vp <- viewport(1, y=1, width=0.7, height=1, just=c("right","top"))
-pushViewport(chart_vp)
-grid.draw(ggplotGrob(myplot))
-popViewport()
-
-ggsave('C:/Users/sbuchhorn/Desktop/gg/ggest5.tiff')
-
-
-# testing
-title <- expression(atop(bold("Change in labor force \n size per 1,000 \n residents, by age, \n Chicago and select \n Metropolitan \n Statistical Areas, \n 2006-10 to 2013-17"),
-                         scriptstyle("This is the caption")))
-
-figure <- myplot
-
-ggpubr::annotate_figure(figure,
-                left = ggpubr::text_grob(title))
-
-
-# hmmm like, how can I group the title and source together
-# https://stackoverflow.com/questions/58160415/how-can-a-subtitle-be-added-to-an-annotate-figure-plot
-# like this is nice but I don't think I can choose the font??
+draw_plot(title, subtitle, "C:/Users/sbuchhorn/desktop/gg/ggtest1.png", 700, 500)
--- R/theme_cmap.R
+++ R/theme_cmap.R
@@ -3,38 +3,26 @@
 #' @export
 theme_cmap <- function() {
   ggplot2::theme(
-
-    #Text format:
-    #This sets the font, size, type and colour of text for the chart's title
-    plot.title = ggplot2::element_text(family=cmapplot_globals$font_title,
-                                       size=28,
-                                       face="bold",
-                                       color="#222222"),
-    #This sets the font, size, type and colour of text for the chart's subtitle, as well as setting a margin between the title and the subtitle
-    plot.subtitle = ggplot2::element_text(family=cmapplot_globals$font_title,
-                                          size=22,
-                                          margin=ggplot2::margin(9,0,9,0)),
-    plot.caption = ggplot2::element_blank(),
-    #This leaves the caption text element empty, because it is set elsewhere in the finalise plot function
-
     #Legend format
     #This sets the position and alignment of the legend, removes a title and backround for it and sets the requirements for any text within the legend. The legend may often need some more manual tweaking when it comes to its exact position based on the plot coordinates.
     legend.position = "top",
+    legend.justification = "left",
     legend.text.align = 0,
     legend.background = ggplot2::element_blank(),
     legend.title = ggplot2::element_blank(),
     legend.key = ggplot2::element_blank(),
     legend.text = ggplot2::element_text(family=cmapplot_globals$font_main,
-                                        size=18,
+                                        size=14,
                                         color="#222222"),
 
     #Axis format
     #This sets the text font, size and colour for the axis test, as well as setting the margins and removes lines and ticks. In some cases, axis lines and axis ticks are things we would want to have in the chart - the cookbook shows examples of how to do so.
     axis.title = ggplot2::element_blank(),
     axis.text = ggplot2::element_text(family=cmapplot_globals$font_main,
-                                      size=18,
+                                      size=14,
                                       color="#222222"),
-    axis.text.x = ggplot2::element_text(margin=ggplot2::margin(5, b = 10)),
+    axis.text.x = ggplot2::element_text(margin=ggplot2::margin(t = 5, r= 30, b = 10, l= 5),
+                                        angle=45, vjust=0.5),
     axis.ticks = ggplot2::element_blank(),
     axis.line = ggplot2::element_blank(),
 
@@ -50,13 +38,7 @@ theme_cmap <- function() {
 
     #Strip background (#This sets the panel background for facet-wrapped plots to white, removing the standard grey ggplot background colour and sets the title size of the facet-wrap title to font size 22)
     strip.background = ggplot2::element_rect(fill="white"),
-    strip.text = ggplot2::element_text(size  = 22,  hjust = 0)
+    strip.text = ggplot2::element_text(size = 14,  hjust = 0)
   )
 }
 
-
-# ### TEST PLOT ###
-# ggplot(ggplot2::diamonds) +
-#   geom_point(aes(x=carat, y=price, color=cut), alpha=0.25) +
-#   labs(title="Diamond price by weight", x="Weight (carat)", y="Price (dollars)") +
-#   theme_cmap()
