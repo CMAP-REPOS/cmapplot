bb1df15fe2d8f9326a5a71b019a80de36d414c62
Matthew Stern <54633946+matthewstern@users.noreply.github.com>
2020-04-07 17:18:03 -0500

    Tweaked format of theme_cmap file
    created font warning if Whitney not available
    shifted to do.call format so that list of theme attributes can be constructed dynamically.

--- R/theme_cmap.R
+++ R/theme_cmap.R
@@ -2,67 +2,69 @@
 #'
 #' @export
 theme_cmap <- function() {
-  ggplot2::theme(
+  # Generate an explicit message to user if Whitney font family is not available
+  if (!(cmapplot_globals$use_whitney)){
+    message("'Whitney' font family not found. Using Calibri...")
+  }
 
-    #Text format:
-    #This sets the default font family
-    text = ggplot2::element_text(family=cmapplot_globals$font_main),
+  # Generate list of theme elements
+  theme_list <- list(
 
-    #This sets the font, size, type and colour of text for the chart's title
+    # Default text
+    text = ggplot2::element_text(family = cmapplot_globals$font_main,
+                                 face = cmapplot_globals$font_main_face,
+                                 size = 12,
+                                 color="#222222"),
+
+    # Title text
     plot.title = ggplot2::element_text(family=cmapplot_globals$font_title,
                                        face=cmapplot_globals$font_title_face,
-                                       size=28,
-                                       color="#222222"),
-    #This sets the font, size, type and colour of text for the chart's subtitle, as well as setting a margin between the title and the subtitle
-    plot.subtitle = ggplot2::element_text(family=cmapplot_globals$font_title,
-                                          face=cmapplot_globals$font_title_face,
-                                          size=22,
-                                          margin=ggplot2::margin(9,0,9,0)),
+                                       size=14),
+
+    # Text elements not displayed
+    plot.subtitle = ggplot2::element_blank(),
     plot.caption = ggplot2::element_blank(),
-    #This leaves the caption text element empty, because it is set elsewhere in the finalise plot function
 
     #Legend format
-    #This sets the position and alignment of the legend, removes a title and backround for it and sets the requirements for any text within the legend. The legend may often need some more manual tweaking when it comes to its exact position based on the plot coordinates.
     legend.position = "top",
     legend.text.align = 0,
     legend.background = ggplot2::element_blank(),
     legend.title = ggplot2::element_blank(),
     legend.key = ggplot2::element_blank(),
-    legend.text = ggplot2::element_text(family=cmapplot_globals$font_main,
-                                        face=cmapplot_globals$font_main_face,
-                                        size=18,
-                                        color="#222222"),
 
     #Axis format
-    #This sets the text font, size and colour for the axis test, as well as setting the margins and removes lines and ticks. In some cases, axis lines and axis ticks are things we would want to have in the chart - the cookbook shows examples of how to do so.
-    axis.title = ggplot2::element_blank(),
-    axis.text = ggplot2::element_text(family=cmapplot_globals$font_main,
-                                      face=cmapplot_globals$font_main_face,
-                                      size=18,
-                                      color="#222222"),
+    axis.title.y = ggplot2::element_blank(),
+    axis.title.x = ggplot2::element_blank(),
     axis.text.x = ggplot2::element_text(margin=ggplot2::margin(5, b = 10)),
     axis.ticks = ggplot2::element_blank(),
     axis.line = ggplot2::element_blank(),
 
     #Grid lines
-    #This removes all minor gridlines and adds major y gridlines. In many cases you will want to change this to remove y gridlines and add x gridlines. The cookbook shows you examples for doing so
-    panel.grid.minor = ggplot2::element_blank(),
     panel.grid.major.y = ggplot2::element_line(color="#cbcbcb"),
     panel.grid.major.x = ggplot2::element_blank(),
 
     #Blank background
-    #This sets the panel background as blank, removing the standard grey ggplot background colour from the plot
     panel.background = ggplot2::element_blank(),
 
-    #Strip background (#This sets the panel background for facet-wrapped plots to white, removing the standard grey ggplot background colour and sets the title size of the facet-wrap title to font size 22)
+    #Strip background
     strip.background = ggplot2::element_rect(fill="white"),
-    strip.text = ggplot2::element_text(size  = 22,  hjust = 0)
+
+    #Facet wrap text
+    strip.text = ggplot2::element_text(size  = 12,  hjust = 0)
   )
+
+  return(do.call(ggplot2::theme,theme_list))
 }
 
 
-# ### TEST PLOT ###
+# ### TEST PLOTS ###
 # ggplot(ggplot2::diamonds) +
 #   geom_point(aes(x=carat, y=price, color=cut), alpha=0.25) +
 #   labs(title="Diamond price by weight", x="Weight (carat)", y="Price (dollars)") +
 #   theme_cmap()
+
+# filter(traded_emp_by_race, variable %in% c("SpecializedTraded", "UnspecializedTraded")) %>%
+#   ggplot(aes(x = reorder(Race, -value), y = value, fill = variable)) +
+#   geom_col(position = position_stack(reverse = TRUE)) +
+#   scale_y_continuous(labels = scales::percent) +
+#   theme_cmap() + ggtitle("A graph")
