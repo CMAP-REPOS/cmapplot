8f3772ceb21ed0db5484286c191b8251a530b09e
sarahcmap <sbuchhorn@cmap.illinois.gov>
2020-09-15 14:24:34 -0500

    adding subtitle height, showing defaults in params

--- R/finalize_plot.R
+++ R/finalize_plot.R
@@ -5,16 +5,16 @@
 #'
 #' @param plot The variable name of the plot you have created that you want to work with
 #' @param title The text you want to appear in the title block.  You can write the title as one string, or add in newline breaks ('\n') with option autowrap = FALSE.
-#' @param subtitle The next you want to appear in the subtitle block.  You can write the subtitle as one string, or add in newline breaks ('\n') with option autowrap = FALSE.
+#' @param subtitle The text you want to appear in the subtitle block.  You can write the subtitle as one string, or add in newline breaks ('\n') with option autowrap = FALSE.
 #' @param save_filepath Filepath you want the plot to be saved to.  You can specify an extension to use, or an extension will be added if you specify 'type'
-#' @param height The height in pixels for the image.
-#' @param type The type of output - use to set extension and give max height warnings.
-#' @param sidepercent The rough percent of total width that the left column shoud take up.
-#' @param autowrap Wrap title and subtitle text automatically
-#' @param titlewraplen The wrap code point length for the title (use with autowrap = TRUE)
-#' @param subtitlewraplen The wrap code point length for the subtitle (use with autowrap = TRUE)
-#' @param action Choose whether to view or save the plot
-#' @param newwindow View plot in a new window if TRUE
+#' @param height The height in pixels for the image.  Default = 400.
+#' @param type The type of output ('ppt', 'word', or 'web'). Use to set file extension and give max height warnings.  Only used when action = save.
+#' @param sidepercent The rough percent of total width that the left column should take up.  Default = 23.
+#' @param autowrap Wrap title and subtitle text automatically.  Default = TRUE.
+#' @param titlewraplen The wrap code point length for the title (use with autowrap = TRUE).  Default = 22.
+#' @param subtitlewraplen The wrap code point length for the subtitle (use with autowrap = TRUE).  Default = 28.
+#' @param action Choose whether to view or save the plot.  Default = view.
+#' @param newwindow View plot in a new window if TRUE.  Default = FALSE.
 #'
 #'
 #' @examples
@@ -26,7 +26,7 @@
 #   scale_y_continuous(labels = scales::percent) + theme_cmap()
 #
 #
-# draw_plot(myplot,title, subtitle, action='save', type='web', save_filepath = '/Users/sarahbuchhorn/Desktop/test6.png')
+# finalize_plot(myplot, title, subtitle, action='save', type='web', save_filepath = '/Users/sarahbuchhorn/Desktop/test6.png')
 
 
 
@@ -81,7 +81,7 @@ create_title_block <- function (title, subtitle) {
                                                                fontface=cmapplot_globals$font_title_face,
                                                                lineheight=0.93)),
                                 grid::textGrob(subtitle,
-                                               x = 0.1, hjust = 0, vjust = 1, y = 0.72,
+                                               x = 0.1, hjust = 0, vjust = 1, y = 0.6,
                                                gp = grid::gpar(fontsize=11,
                                                                fontfamily=cmapplot_globals$font_reg,
                                                                fontface=cmapplot_globals$font_reg_face,
@@ -93,7 +93,7 @@ create_title_block <- function (title, subtitle) {
 }
 
 #' @export
-draw_plot <- function(plot,
+finalize_plot <- function(plot,
                       title,
                       subtitle,
                       save_filepath,
