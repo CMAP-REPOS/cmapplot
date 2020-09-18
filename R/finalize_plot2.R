122a0fc97d15fc7036e747be6576543b905fa12c
Daniel <daniel@danielcomeaux.com>
2020-09-18 11:12:33 -0500

    New functionality
    PDF saving, fix line graph widths, and adding "grid::" prefix to functions. Note significant additions around font families, which we will want to review with Noel in some more detail before merging with the master.

--- R/finalize_plot2.R
+++ R/finalize_plot2.R
@@ -6,20 +6,28 @@
 # myplot <- ggplot(economy_basic, aes(x = interaction(year, variable), y = value, fill = sector)) +
 #   geom_col(position = "fill") +
 #   scale_y_continuous(labels = scales::percent) + theme_cmap()
+# #
+# finalize_plot2(myplot, "title is<br>long so it might take many lines", "subtitle keeps going, and going, and going!", mode = "svg", filepath = "foo.svg")
 #
-# finalize_plot2(myplot, "title is<br>long so it might take many lines", "subtitle keeps going and going!", mode = "png", filepath = "foo.png")
-
-
-
-# # testing testing testing
+#
+#
+# # # testing testing testing
 # title <- "Change in labor force size per 1,000 residents, by age, Chicago and select Metropolitan Statistical Areas, 2006-10 to 2013-17"
 # subtitle <- "Source: Chicago Metropolitan Agency for Planning analysis of National Highway Traffic Safety Administration Corporate Average Fuel Economy Fact Sheets; Illinois Department of Transportation data; 2009 National Household Travel Survey data."
 #
 #
-# finalize_plot2(myplot,title,subtitle,mode="save",save_filepath = "foo.png",type = "ppt", height = 400)
-
-
-
+# finalize_plot2(myplot,title,subtitle,filepath = "foo",mode = "pdf", height = 400)
+#
+#
+# myplot2 <- transit_ridership %>%
+#   ggplot(aes(x = year, y = ridership, color = system)) +
+#   geom_line() +
+#   theme_cmap()
+#
+# title2 <- "Transit ridership in the RTA region over time, 1980-2019 (in millions)"
+# subtitle2 <- "Source: Chicago Metropolitan Agency for Planning analysis of data from the Regional Transit Authority"
+#
+# finalize_plot2(myplot2,title2,subtitle2,filepath = "too", mode="png")
 
 ### Actual code below
 
@@ -28,7 +36,7 @@
 finalize_plot2 <- function(plot = ggplot2::last_plot(),
                            title = "Title here",
                            subtitle = "Subtitle here",
-                           mode = c("plot", "newwindow", "object", "svg", "png", "tiff"),
+                           mode = c("plot", "newwindow", "object", "svg", "png", "tiff","pdf"),
                            width = 670,
                            height = 400,
                            title_width = 150,
@@ -41,7 +49,7 @@ finalize_plot2 <- function(plot = ggplot2::last_plot(),
   mode <- match.arg(mode)
 
   # validate output details
-  savetypes <- c("svg", "png", "tiff")
+  savetypes <- c("svg", "png", "tiff","pdf")
   if(mode %in% savetypes){
     # check for filepath
     if(is.null(filepath)){stop("You must specify a filepath in save mode")}
@@ -65,43 +73,54 @@ finalize_plot2 <- function(plot = ggplot2::last_plot(),
 
   # function constants
   # (***should these be function arguments?***)
-  c_topbar_lwd <- unit(3,"bigpts") # width of top line
-  c_textmargin_left <- unit(2,"bigpts") # margin to left of title and subtitle text
-  c_textmargin_top <- unit(5,"bigpts") # margin between top line and title
-  c_textmargin_mid <- unit(10,"bigpts") # margin between title and subtitle
-  c_plotmargins <- unit(c(5,5,5,10),"bigpts") #margins around plot, clockwise from top
+  c_topbar_lwd <- grid::unit(3,"bigpts") # width of top line
+  c_textmargin_left <- grid::unit(2,"bigpts") # margin to left of title and subtitle text
+  c_textmargin_top <- grid::unit(5,"bigpts") # margin between top line and title
+  c_textmargin_mid <- grid::unit(10,"bigpts") # margin between title and subtitle
+  c_plotmargins <- grid::unit(c(5,5,5,10),"bigpts") #margins around plot, clockwise from top
 
-  # modify the plot a bit
+  # modify the plot a bit to fix margins
   plot <- plot + theme(plot.margin = c_plotmargins,
                        plot.title = ggplot2::element_blank())
 
-  # TO BE ADDED : Size conversion for widths in line graphs
+  # FLAG FOR REVIEW BY NOEL - Fix discrepancy in fonts for exporting to svg and pdf
+  if (.Platform$OS.type == "windows") {
+    plot <- plot + theme(text = element_text(family = windowsFonts(cmapplot_globals$font_main)))
+  } else {
+    plot <- plot + theme(text = element_text(family = X11Fonts(cmapplot_globals$font_main)))
+  }
 
+  # Size conversion for widths in line graphs
+  old_lwd <- GeomLine$default_aes$size
+  update_geom_defaults("line", list(size = cmapplot_globals$lwd_layout))
 
   # Build necessary grobs -----------------------------------------------------
 
   # rectangle grob for top line
-  grob_topline <- grid::rectGrob(gp=gpar(fill = cmapplot_globals$colors$blackish,
+  grob_topline <- grid::rectGrob(gp=grid::gpar(fill = cmapplot_globals$colors$blackish,
                                          col = "white",
                                          lwd=0))
 
   # title grob
   grob_title <- gridtext::textbox_grob(text = title,
                                        # set top left location of grob
-                                       x = unit(0, "npc"),
-                                       y = unit(1, "npc"),
+                                       x = grid::unit(0, "npc"),
+                                       y = grid::unit(1, "npc"),
                                        hjust = 0,
                                        vjust = 1,
                                        # set margins within textbox
-                                       padding = unit.c(c_textmargin_top, # top
-                                                        unit(1, "bigpts"), # right
-                                                        unit(1, "bigpts"), # bottom
-                                                        c_textmargin_left),# left
+                                       padding = grid::unit.c(c_textmargin_top, # top
+                                                              grid::unit(1, "bigpts"), # right
+                                                              grid::unit(1, "bigpts"), # bottom
+                                                              c_textmargin_left),# left
                                        # set aesthetic variables
-                                       gp = gpar(fontsize=cmapplot_globals$font_sizes$title,
-                                                 fontfamily=cmapplot_globals$font_title,
-                                                 fontface=cmapplot_globals$font_title_face,
-                                                 lineheight=0.93)
+                                       gp = grid::gpar(fontsize=cmapplot_globals$font_sizes$title,
+                                                       # FLAG FOR REVIEW BY NOEL - fix for font family that was causing problems in SVG and PDF exports
+                                                       fontfamily= if (.Platform$OS.type == "windows") {
+                                                         windowsFonts(cmapplot_globals$font_title)} else {
+                                                            X11Fonts(cmapplot_globals$font_title)},
+                                                       fontface=cmapplot_globals$font_title_face,
+                                                       lineheight=0.93)
                                        # # for debug, draw box around grob
                                        # , box_gp = gpar(col = "blue", fill = "cornsilk")
                                        )
@@ -109,20 +128,23 @@ finalize_plot2 <- function(plot = ggplot2::last_plot(),
   # subtitle grob
   grob_subtitle <-  gridtext::textbox_grob(text = subtitle,
                                            # set top left location of grob
-                                           x = unit(0, "npc"),
-                                           y = unit(1, "npc") - grid::grobHeight(grob_title),
+                                           x = grid::unit(0, "npc"),
+                                           y = grid::unit(1, "npc") - grid::grobHeight(grob_title),
                                            hjust = 0,
                                            vjust = 1,
                                            # set margins within textbox
-                                           padding = unit.c(c_textmargin_mid, # top
-                                                            unit(1, "bigpts"), # right
-                                                            unit(1, "bigpts"), # bottom
-                                                            c_textmargin_left),# left
+                                           padding = grid::unit.c(c_textmargin_mid, # top
+                                                                  grid::unit(1, "bigpts"), # right
+                                                                  grid::unit(1, "bigpts"), # bottom
+                                                                  c_textmargin_left),# left
                                            # set aesthetic variables
-                                           gp = gpar(fontsize=cmapplot_globals$font_sizes$note,
-                                                     fontfamily=cmapplot_globals$font_reg,
-                                                     fontface=cmapplot_globals$font_reg_face,
-                                                     lineheight=0.93)
+                                           gp = grid::gpar(fontsize=cmapplot_globals$font_sizes$note,
+                                                           # FLAG FOR REVIEW BY NOEL - fix for font family that was causing problems in SVG and PDF exports
+                                                           fontfamily= if (.Platform$OS.type == "windows") {
+                                                             windowsFonts(cmapplot_globals$font_main)} else {
+                                                               X11Fonts(cmapplot_globals$font_main)},
+                                                           fontface=cmapplot_globals$font_main_face,
+                                                           lineheight=0.93)
                                            # # for debug, draw box around grob
                                            # , box_gp = gpar(col = "blue", fill = "lavenderblush")
   )
@@ -140,13 +162,14 @@ finalize_plot2 <- function(plot = ggplot2::last_plot(),
                                                 grobTree(grob_title, grob_subtitle),
                                                 plot),
                                    layout_matrix = layout,
-                                   heights = unit.c(c_topbar_lwd, unit(height, "bigpts") - c_topbar_lwd),
-                                   widths = unit(c(title_width, width - title_width), "bigpts"))
+                                   heights = grid::unit.c(c_topbar_lwd,
+                                                          unit(height, "bigpts") - c_topbar_lwd),
+                                   widths = grid::unit(c(title_width, width - title_width),
+                                                       "bigpts"))
 
 
   # # as debug tool, print viewport layouts to plot window
   # gtable::gtable_show_layout(output)
-
 
   # output the figure based on user setting -----------------------------------
 
@@ -160,7 +183,10 @@ finalize_plot2 <- function(plot = ggplot2::last_plot(),
                     width = (width/72),
                     height=(height/72),
                     dpi = 72,
-                    bg="white")
+                    bg="white",
+                    # If PDF, switch device to "cairo" for better PDF handling
+                    device = if (mode == "pdf") {cairo_pdf} else {mode}
+                    )
     message("Export successful")
   } else if (mode == "plot") {
     # OR print the grob to the plots window
@@ -181,6 +207,8 @@ finalize_plot2 <- function(plot = ggplot2::last_plot(),
     }
   }
 
+  # return geom defaults as before
+  update_geom_defaults("line",list(size = old_lwd))
 }
 
 
