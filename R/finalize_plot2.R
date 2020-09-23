03b59e3b532f598864c567580a4ebc43bd4ab9ab
Noel Peterson <nmpeterson@users.noreply.github.com>
2020-09-23 10:54:13 -0500

    If title/caption unspecified, try to use input plot's
    Also cleaned up some code formatting

--- R/finalize_plot2.R
+++ R/finalize_plot2.R
@@ -1,23 +1,23 @@
 #'Arrange and save CMAP ggplot chart
 #'
 #'\code{finalize_plot2} will save a ggplot into a frame defined by  CMAP design
-#'standards. It will align your title and subtitle to the left, add a horizontal
+#'standards. It will align your title and caption to the left, add a horizontal
 #'line on top, and make other adjustments. If instructed to do so, it will also
 #'save the plot in one of four file formats (png, tiff, svg, and pdf) to a
 #'specified location. This function will not apply CMAP design standards to the
 #'plot itself: use with \code{theme_cmap()} for that.
 #'
-#'@usage finalize_plot2(plot = ggplot2::last_plot(),title = "Title here",
-#'  subtitle = "Subtitle here",mode = c("plot", "newwindow", "object", "svg",
-#'  "png", "tiff","pdf"),width = 670,height = 400,title_width = 150,
-#'  filepath = NULL,plot_margins = c(5,5,5,10),top_bar = 2,text_margin_left = 2,
-#'  text_margin_top = 5,text_margin_mid = 10)
+#'@usage finalize_plot2(plot = ggplot2::last_plot(), title = "Title here",
+#'  caption = "Caption here", mode = c("plot", "newwindow", "object", "svg",
+#'  "png", "tiff", "pdf"), width = 670, height = 400, title_width = 150,
+#'  filepath = NULL, plot_margins = c(5,5,5,10), top_bar = 2, text_margin_left
+#'  = 2, text_margin_top = 5, text_margin_mid = 10)
 #'
 #'@param plot ggplot object, the variable name of the plot you have created that
 #'  you want to finalize. The efault is \code{ggplot2::last_plot()}, so if
 #'  unspecified, the most recent plot will be used.
 #'@param title Char, the text you want to appear in the title block.
-#'@param subtitle Char, the text you want to appear in the subtitle block.
+#'@param caption Char, the text you want to appear in the caption block.
 #'@param mode Char, the action to be taken with the plot. Default is to view in
 #'  the window (using "plot"). Other options include "newwindow" (displays in a
 #'  new window), "object" (returns a grob object, but does not print), or one of
@@ -36,11 +36,11 @@
 #'  respectively. Inputs should be formatted as c(`top`,`right`,`bottom`,`left`)
 #'@param top_bar Numeric, the width of the line above the title and graph in
 #'  big points. Default is 3 "big points."
-#'@param text_margin_left Unit, the margin to left of title and subtitle text.
+#'@param text_margin_left Unit, the margin to left of title and caption text.
 #'  Default is 2 "big points."
 #'@param text_margin_top Unit, the margin between the top line and title text.
 #'  Default is 5 "big points."
-#'@param text_margin_mid Unit, the margin between the title and subtitle text.
+#'@param text_margin_mid Unit, the margin between the title and caption text.
 #'  Default is 10 "big points."
 #'
 #'@importFrom grid gpar unit unit.c grobTree
@@ -88,9 +88,9 @@
 #'                width = 800)
 #'}
 #'@export
-finalize_plot2 <- function(plot = ggplot2::last_plot(),
+finalize_plot2 <- function(input_plot = ggplot2::last_plot(),
                            title = "Title here",
-                           subtitle = "Subtitle here",
+                           caption = "Caption here",
                            mode = c("plot", "newwindow", "object", "svg", "png", "tiff","pdf"),
                            width = 670,
                            height = 400,
@@ -109,21 +109,21 @@ finalize_plot2 <- function(plot = ggplot2::last_plot(),
   mode <- match.arg(mode)
 
   # check plot margins argument
-  if (length(plot_margins) != 4){stop("Plot margins must be a vector of length four")}
+  if (length(plot_margins) != 4) { stop("Plot margins must be a vector of length four") }
 
   # validate output details
   savetypes <- c("svg", "png", "tiff","pdf")
-  if(mode %in% savetypes){
+  if (mode %in% savetypes) {
     # check for filepath
-    if(is.null(filepath)){stop("You must specify a filepath in save mode")}
+    if (is.null(filepath)) { stop("You must specify a filepath in save mode") }
 
     # if extension does not contain correct extension, add it
-    if(!(grepl(paste0("\\.",mode, "$"), filepath))){
+    if (!(grepl(paste0("\\.", mode, "$"), filepath))) {
       filepath <- paste0(filepath, ".", mode)
     }
 
     # if mode is SVG, confirm svglite package.
-    if(mode == "svg"){
+    if (mode == "svg") {
       if (!("svglite" %in% rownames(utils::installed.packages()))) {
         stop("To export as SVG, package `svglite` is required.")
       }
@@ -131,99 +131,117 @@ finalize_plot2 <- function(plot = ggplot2::last_plot(),
   }
 
   # validate height and width
-  if(width != 670){warning("Width should typically be 670 px exactly.")}
-  if(height > 400){warning("Height should typically be 400 px or less.")}
+  if (width != 670) { warning("Width should typically be 670 px exactly.") }
+  if (height > 400) { warning("Height should typically be 400 px or less.") }
 
   # validate top bar
-  if(top_bar != 2){warning("Top bar should typically be 2 big points exactly")}
+  if (top_bar != 2) { warning("Top bar should typically be 2 big points exactly") }
 
   # validate margins
-  if(text_margin_left != 2){warning("Margin between left edge and title text should typically be 2 big points exactly")}
-  if(text_margin_top != 5){warning("Margin between top bar and title should typically be 5 big points exactly")}
-  if(text_margin_mid != 10){warning("Margin between title and subtitle should typically be 10 big points exactly")}
+  if (text_margin_left != 2) { warning("Margin between left edge and title text should typically be 2 big points exactly") }
+  if (text_margin_top != 5) { warning("Margin between top bar and title should typically be 5 big points exactly") }
+  if (text_margin_mid != 10) { warning("Margin between title and caption should typically be 10 big points exactly") }
 
 
   # convert top bar and margins to big points
   # At 72 dpi, 2.001 big points appears to be the minimum that will render
   if ((mode == "png" | mode == "tiff") & top_bar <= 2 & top_bar > 0) {
-    top_bar <- grid::unit(2.001,"bigpts")
+    top_bar <- grid::unit(2.001, "bigpts")
   } else {
     top_bar <- top_bar <- grid::unit(top_bar,"bigpts")
   }
-  text_margin_left <- grid::unit(text_margin_left,"bigpts")
-  text_margin_top <- grid::unit(text_margin_top,"bigpts")
-  text_margin_mid <- grid::unit(text_margin_mid,"bigpts")
-  plot_margins <- grid::unit(plot_margins,"bigpts")
+  text_margin_left <- grid::unit(text_margin_left, "bigpts")
+  text_margin_top <- grid::unit(text_margin_top, "bigpts")
+  text_margin_mid <- grid::unit(text_margin_mid, "bigpts")
+  plot_margins <- grid::unit(plot_margins, "bigpts")
 
+
+  # If title/caption unspecified, try to extract from plot
+  input_title = input_plot$labels$title
+  if (title == "Title here" & !is.null(input_title)) {
+    title = input_title
+  }
+
+  input_caption = input_plot$labels$caption
+  if (caption == "Caption here" & !is.null(input_caption)) {
+    caption = input_caption
+  }
 
 
   # FLAG FOR REVIEW AND FURTHER THOUGHT - modify the plot a bit to fix margins
   # and text size on plot
-  plot <- plot + ggplot2::theme(plot.margin = plot_margins,
-                                plot.title = ggplot2::element_blank(),
-                                text = ggplot2::element_text(size = cmapplot_globals$font_sizes$main * 1.25))
-                       # The plot appears to be resizing when exported via save
-                       # (but not in plot or newwindow). To have correct font
-                       # sizes for export, it appears that chart text must be
-                       # specified at 17.5 (which is 1.25 x 14, or 96/72 x 14)
-                       # appears to be accurately rendered at 14. This could be
-                       # an artifact of the points vs. pixels size problem.
+  input_plot <- input_plot + ggplot2::theme(
+    plot.margin = plot_margins,
+    plot.title = ggplot2::element_blank(),
+    text = ggplot2::element_text(size = cmapplot_globals$font_sizes$main * 1.25)
+  )
+
+  # The plot appears to be resizing when exported via save
+  # (but not in plot or newwindow). To have correct font
+  # sizes for export, it appears that chart text must be
+  # specified at 17.5 (which is 1.25 x 14, or 96/72 x 14)
+  # appears to be accurately rendered at 14. This could be
+  # an artifact of the points vs. pixels size problem.
 
   # Size conversion for widths in line graphs (this is ignored in calls that
   # return a grob object, as it is not yet drawn)
   if (mode != "object") {
-  default_lwd <- ggplot2::GeomLine$default_aes$size
-  ggplot2::update_geom_defaults("line", list(size = cmapplot_globals$lwd_layout))
+    default_lwd <- ggplot2::GeomLine$default_aes$size
+    ggplot2::update_geom_defaults("line", list(size = cmapplot_globals$lwd_layout))
   }
 
   # Build necessary grobs -----------------------------------------------------
 
   # rectangle grob for top line
-  grob_topline <- grid::rectGrob(gp=grid::gpar(fill = cmapplot_globals$colors$blackish,
-                                               col = "white"))
+  grob_topline <- grid::rectGrob(
+    gp=grid::gpar(fill = cmapplot_globals$colors$blackish,
+                  col = "white")
+  )
 
   # title grob
-  grob_title <- gridtext::textbox_grob(text = title,
-                                       # set top left location of grob
-                                       x = grid::unit(0, "npc"),
-                                       y = grid::unit(1, "npc"),
-                                       hjust = 0,
-                                       vjust = 1,
-                                       # set margins within textbox
-                                       padding = grid::unit.c(text_margin_top, # top
-                                                              grid::unit(1, "bigpts"), # right
-                                                              grid::unit(1, "bigpts"), # bottom
-                                                              text_margin_left),# left
-                                       # set font aesthetic variables
-                                       gp = grid::gpar(fontsize=cmapplot_globals$font_sizes$title,
-                                                       fontfamily=cmapplot_globals$font_title,
-                                                       fontface=cmapplot_globals$font_title_face,
-                                                       lineheight=0.93,
-                                                       col=cmapplot_globals$colors$blackish)
-                                       # # for debug, draw box around grob
-                                       # , box_gp = gpar(col = "blue", fill = "cornsilk")
-                                       )
+  grob_title <- gridtext::textbox_grob(
+    text = title,
+    # set top left location of grob
+    x = grid::unit(0, "npc"),
+    y = grid::unit(1, "npc"),
+    hjust = 0,
+    vjust = 1,
+    # set margins within textbox
+    padding = grid::unit.c(text_margin_top, # top
+                           grid::unit(1, "bigpts"), # right
+                           grid::unit(1, "bigpts"), # bottom
+                           text_margin_left), # left
+    # set font aesthetic variables
+    gp = grid::gpar(fontsize=cmapplot_globals$font_sizes$title,
+                    fontfamily=cmapplot_globals$font_title,
+                    fontface=cmapplot_globals$font_title_face,
+                    lineheight=0.93,
+                    col=cmapplot_globals$colors$blackish)
+                    ## for debug, draw box around grob
+                    #, box_gp = gpar(col = "blue", fill = "cornsilk")
+  )
 
-  # subtitle grob
-  grob_subtitle <-  gridtext::textbox_grob(text = subtitle,
-                                           # set top left location of grob
-                                           x = grid::unit(0, "npc"),
-                                           y = grid::unit(1, "npc") - grid::grobHeight(grob_title),
-                                           hjust = 0,
-                                           vjust = 1,
-                                           # set margins within textbox
-                                           padding = grid::unit.c(text_margin_mid, # top
-                                                                  grid::unit(1, "bigpts"), # right
-                                                                  grid::unit(1, "bigpts"), # bottom
-                                                                  text_margin_left), # left
-                                           # set aesthetic variables
-                                           gp = grid::gpar(fontsize = cmapplot_globals$font_sizes$note,
-                                                           fontfamily = cmapplot_globals$font_note,
-                                                           fontface = cmapplot_globals$font_note_face,
-                                                           lineheight = 0.93,
-                                                           col = cmapplot_globals$colors$blackish)
-                                           # # for debug, draw box around grob
-                                           # , box_gp = gpar(col = "blue", fill = "lavenderblush")
+  # Caption grob
+  grob_caption <- gridtext::textbox_grob(
+    text = caption,
+    # set top left location of grob
+    x = grid::unit(0, "npc"),
+    y = grid::unit(1, "npc") - grid::grobHeight(grob_title),
+    hjust = 0,
+    vjust = 1,
+    # set margins within textbox
+    padding = grid::unit.c(text_margin_mid, # top
+                           grid::unit(1, "bigpts"), # right
+                           grid::unit(1, "bigpts"), # bottom
+                           text_margin_left), # left
+    # set aesthetic variables
+    gp = grid::gpar(fontsize = cmapplot_globals$font_sizes$note,
+                    fontfamily = cmapplot_globals$font_note,
+                    fontface = cmapplot_globals$font_note_face,
+                    lineheight = 0.93,
+                    col = cmapplot_globals$colors$blackish)
+                    ## for debug, draw box around grob
+                    #, box_gp = gpar(col = "blue", fill = "lavenderblush")
   )
 
 
@@ -234,16 +252,16 @@ finalize_plot2 <- function(plot = ggplot2::last_plot(),
                  c(2,1))
 
   # stitch together the final plot
-  output <- gridExtra::arrangeGrob(grobs = list(plot,
-                                                grid::grobTree(grob_title, grob_subtitle),
-                                                grob_topline),
-                                   layout_matrix = layout,
-                                   # establish specific dimensions for each column and row
-                                   heights = grid::unit.c(top_bar,
-                                                          unit(height, "bigpts") - top_bar),
-                                   widths = grid::unit(c(title_width,
-                                                         width - title_width),
-                                                         "bigpts"))
+  output <- gridExtra::arrangeGrob(
+    grobs = list(input_plot,
+                 grid::grobTree(grob_title, grob_caption),
+                 grob_topline),
+    layout_matrix = layout,
+    # establish specific dimensions for each column and row
+    heights = grid::unit.c(top_bar,
+                           unit(height, "bigpts") - top_bar),
+    widths = grid::unit(c(title_width, width - title_width), "bigpts")
+  )
 
 
   # # as debug tool, print viewport layouts to plot window
