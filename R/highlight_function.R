53b9562739252710a6a9428d07f35e4614daa74f
Matthew Stern <54633946+matthewstern@users.noreply.github.com>
2020-10-02 20:02:07 -0500

    allow for multiple highlight fields and colors
    new functionality allows for the user to specify multiple fields to highlight at the same time (and multiple colors, if desired).  New examples and documentation text describe this feature.

--- R/highlight_function.R
+++ R/highlight_function.R
@@ -1,8 +1,12 @@
-#' Highlight one discrete group in a comparison graph
+#' Highlight one or more discrete groups in a comparison graph
 #'
 #' Pick the function depending on the aesthetic of your ggplot object (fill or
-#' color). Specify the unique factor name of the group you're highlighting (as a
-#' case-sensitive string), and the vector it's found in.
+#' color). Specify the unique factor name(s) of the group(s) you're highlighting
+#' (as a case-sensitive string), and the vector it's found in.
+#'
+#' You may specify multiple groups to highlight. If you do, you may specify a
+#' single highlight color, or a vector of highlight colors of equal length to
+#' the vector of highlight values.
 #'
 #' This function does not make any modifications to the legend, so legend
 #' behavior is not perfect out-of-the-box. For example, if the plot aesthetic
@@ -14,15 +18,30 @@
 #' @param field character vector, the vector in which the value to highlight is
 #'   found. Values need not be unique. Typically, pass the table column that
 #'   defines the color/fill aesthetic as 'table$field'
-#' @param value character string, the name of group to highlight
-#' @param color_value Specify the highlighted color. Default is #009ccc (blue)
+#' @param value character string or vector, the name of group(s) to highlight
+#' @param color_value Specify the highlight color(s). Default is #009ccc (blue)
 #' @param color_other Specify non-highlighted color. Default is #9daab3 (gray)
 #'
 #' @examples
+#' # one value, default colors
 #' ggplot(dplyr::filter(transit_ridership, year=="2019"),
 #'        aes(x = system, y = ridership, fill = system)) +
 #'    geom_col() +
 #'    cmap_fill_highlight(transit_ridership$system, "metra")
+#'
+#' # multiple values, default colors
+#' ggplot(dplyr::filter(transit_ridership, year=="2019"),
+#'        aes(x = system, y = ridership, fill = system)) +
+#'    geom_col() +
+#'    cmap_fill_highlight(transit_ridership$system, c("metra", "pace_ada"))
+#'
+#' # multiple values, multiple colors
+#' ggplot(dplyr::filter(transit_ridership, year=="2019"),
+#'        aes(x = system, y = ridership, fill = system)) +
+#'    geom_col() +
+#'    cmap_fill_highlight(transit_ridership$system,
+#'    c("metra", "pace_ada"),
+#'    c("red", "orange"))
 #'
 #' @describeIn cmap_fill_highlight For fill aesthetic
 #' @export
@@ -66,13 +85,27 @@ cmap_colour_highlight <- cmap_color_highlight
 #' @noRd
 make_highlight_palette <- function(field, value, color_value, color_other){
 
+  # check that value and color_value are the same length.
+  # if color_value has a length of 1, it will be repeated
+  if(length(value) > 1){
+    if (length(color_value) == 1){
+      color_value <- rep.int(color_value, length(value))
+    } else if(length(value) != length(color_value)){
+      stop("Length of `value` and `color_value` must be equal.", .call = FALSE)
+    }
+  }
+
+  # identify palette length
   n <- length(unique(field))
 
+  # construct initial palette
   palette <- rep.int(c(other = color_other), n)
-
   names(palette) <- levels(factor(field))
 
-  palette[[value]] <- color_value
+  # replace highlight value(s) with highlight color(s)
+  for(i in seq_along(value)){
+    palette[[value[i]]] <- color_value[i]
+  }
 
   return(palette)
 
