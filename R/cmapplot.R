aa8abf0e5235876073a8764ba4a82f5520cd481f
Matthew Stern <54633946+matthewstern@users.noreply.github.com>
2020-11-02 13:04:12 -0600

    documentation & margins ppt/png update

--- R/cmapplot.R
+++ R/cmapplot.R
@@ -94,32 +94,25 @@ cmapplot_globals <- list(
   use_whitney = FALSE,
 
   ## Establish plotting constants in bigpts (1/72 of inch)
-  ## [t] and [f] in comments indicate whether the constant is called in
-  ## [t]heme_cmap() and/or [f]inalize_plot().
   consts = list(
-    lwd_originline = 1.6, # [t, ] Width of origin lines. This appears to be the
-                          #        minimum for variation between origin lines and
-                          #        other background lines
-    lwd_gridline = 0.3,   # [t, ] Width of most lines in plot theme, including any gridlines
-    lwd_plotline = 3,     # [ ,f] Width of line graph lines
-    lwd_topline = 2,      # [ ,f] Width of top line
-    margin_topline_t = 5, # [ ,f] Margin between top edge of image and top line
-    margin_title_t = 5,   # [ ,f] Margin between top line and title
-    margin_title_b = 5,   # [ ,f] Margin between title and caption if
-                          #        `caption_valign = "top"`
-    margin_caption_b = 5, # [ ,f] Margin between caption and bottom edge of image
-    margin_legend_t = 5,  # [ ,f] Margin between top line and plotbox
-    margin_legend_i = 8,  # [t,f] Vertical margin between legends (only applies
-                          #        to multilegend plots)
-    margin_legend_b = 10, # [t,f] Margin between legend and plot (within plotbox)
-    margin_plot_b = 5,    # [ ,f] Margin between plotbox and bottom edge of image
-    margin_title_l = 2,   # [ ,f] Margin between left edge of image and title/caption
-    margin_title_r = 10,  # [ ,f] Margin between title/caption and legend/plot
-                          #        (Both title_l and title_r are deducted from `titlewidth`)
-    margin_plot_r = 10,   # [ ,f] Margin between plotbox and right edge of image
-    margin_panel_r = 20,  # [t,f] Margin between the right side of the plot and plotbox edge
-    leading_title = 1,    # [ ,f] Text leading for Title text
-    leading_caption = 1   # [ ,f] Text leading for Caption text
+    lwd_originline = 1.6,
+    lwd_gridline = 0.3,
+    lwd_plotline = 3,
+    lwd_topline = 2,
+    margin_topline_t = 5,
+    margin_title_t = 5,
+    margin_title_b = 5,
+    margin_caption_b = 5,
+    margin_legend_t = 5,
+    margin_legend_i = 8,
+    margin_legend_b = 10,
+    margin_plot_b = 5,
+    margin_title_l = 2,
+    margin_title_r = 10,
+    margin_plot_r = 10,
+    margin_panel_r = 20,
+    leading_title = 1,
+    leading_caption = 1
   )
 )
 
