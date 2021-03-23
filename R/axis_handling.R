e1bba87dcc2d71b11afde9604452db0277e893cc
Daniel Comeaux <DComeaux@cmap.illinois.gov>
2021-03-23 18:29:14 -0500

    Revert "Delete integer_breaks"
    This reverts commit 1a3f2708b16499446c2af3a06bab72e40029b94b.

--- R/axis_handling.R
+++ R/axis_handling.R
@@ -25,23 +25,23 @@ abbreviate_dates <- function(breaks,
     # Determine length of each break
     lengths <- stringr::str_length(as.integer(breaks))
 
-    # # Stop if the breaks are not in a four-digit format - this seems to cause errors with pretty breaks
-    # if (!(min(lengths == 4) & max(lengths == 4))) {
-    #   stop("Breaks are not in a four-digit format. Cannot abbreviate.")
-    # }
-    #
-    # # Stop if breaks cannot be coerced to a number
-    # tryCatch({as.integer(breaks)},
-    #          warning = function(w){stop("Breaks cannot be coerced to a year. Cannot abbreviate.")})
+    # Stop if the breaks are not in a four-digit format
+    if (!(min(lengths == 4) & max(lengths == 4))) {
+      stop("Breaks are not in a four-digit format. Cannot abbreviate.")
+    }
+
+    # Stop if breaks cannot be coerced to a number
+    tryCatch({as.integer(breaks)},
+             warning = function(w){stop("Breaks cannot be coerced to a year. Cannot abbreviate.")})
 
     # Abbreviate all values
     abbr <- paste0("'",substr(breaks,3,4))
 
     # Add back any specified non-abbreviations
     if(!is.null(full_by_date)) {
-      full_by_pos <- sort(unique(c(full_by_pos,match(breaks,full_by_date))))
+      full_by_pos <- arrange(distinct(c(full_by_pos,match(breaks,full_by_date))))
     }
- ##### NOTE - there are issues with pretty breaks, it increments the adjusted labels up by one. Not sure what's going on.
+
     if(!is.null(full_by_pos)) {
       abbr[full_by_pos] <- breaks[full_by_pos]
     }
