013787bbeac492d47377b356c811c4f0bf4b0f5d
Matthew Stern <mstern@cmap.illinois.gov>
2021-04-16 16:46:24 -0500

    typo

--- R/cmapplot.R
+++ R/cmapplot.R
@@ -40,9 +40,9 @@
     purrr::walk2(user_font_names, user_font_paths, systemfonts::register_font)
 
     registry_fonts <- systemfonts::registry_fonts()
-    message(paste(registry_fonts, collapse = "\n")) ## temp
+    message(length(registry_fonts$family[registry_fonts$family %in% whitney_core])) ## temp
     assign("use_whitney",
-           length(registry_fonts$name[registry_fonts$name %in% whitney_core]) >= 3,
+           length(registry_fonts$family[registry_fonts$family %in% whitney_core]) >= 3,
            envir = cmapplot_globals)
   }
 
